2f80c5abe8d3edbfd5835ab89e04168a6a2cc976
vuxuanquan <quanvu143@gmail.com>
2024-12-10 22:48:56 +0700

    feat: first commit

--- src/main/java/com/quanvx/esim/controller/JoytelController.java
+++ src/main/java/com/quanvx/esim/controller/JoytelController.java
@@ -1,11 +1,16 @@
 package com.quanvx.esim.controller;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
 
 @RestController
+@Slf4j
 public class JoytelController {
 
     // Inject RestTemplate để gửi HTTP request
@@ -17,5 +22,11 @@ public class JoytelController {
     public String gateway(@RequestParam String url) {
         // Gọi API từ URL đã nhận và trả về kết quả
         return restTemplate.getForObject(url, String.class);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello() {
+        // Gọi API từ URL đã nhận và trả về kết quả
+        return ResponseEntity.ok().body("hello");
     }
 }
